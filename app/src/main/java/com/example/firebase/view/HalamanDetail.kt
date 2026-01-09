fb34a74da280f8fb03e9f150556a2b8c1e8f452a
Putranugroho14 <putranugroho61@gmail.com>
2026-01-09 14:48:55 +0700

    buat kelanjutan fun body detail data siswa

--- app/src/main/java/com/example/firebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase/view/HalamanDetail.kt
@@ -100,7 +100,41 @@ private fun BodyDetailDataSiswa(
     statusUiDetail: StatusUIDetail,
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
-)
+) {
+    Column(
+        modifier = modifier
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        when (statusUiDetail) {
+            is StatusUIDetail.Success -> DetailDataSiswa(
+                siswa = statusUiDetail.siswa,
+                modifier = Modifier.fillMaxWidth()
+            )
+            else -> {}
+        }
+
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
 }
 
 
